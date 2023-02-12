c3df59c17fe44d70053535a5fac4452293744891
GokceUzun <91784437+GokceUzun@users.noreply.github.com>
2023-02-12 17:58:25 +0000

    how loading and filtering is combined

--- Preprocess.py
+++ Preprocess.py
@@ -1,26 +1,34 @@
 '''Module that combines both preprocessing steps loading and filtering'''
 
+import numpy as np
 from LoadData import LoadData
 from Filter import Filter
 
-#directory = '/Volumes/Gonzalez-Sulser/SYNGAP SLEEP 24hr PAPER/Circadian-ETX/TAINI files/S7072'
-#filename = "TAINI_1033_S7072_Drug2-2020_03_23-0000.dat"
-#unfiltered_data = LoadData(filename, directory).parse_dat() 
+directory = '/Volumes/Gonzalez-Sulser/SYNGAP SLEEP 24hr PAPER/Circadian-ETX/TAINI files/S7072'
+filename = "TAINI_1033_S7072_Drug2-2020_03_23-0000.dat"
+#path_to_save_folder = '/Volumes/Macintosh HD/Users/gokceuzun/Desktop/4. SENE/Honors Project/S7072'
+#save_as_name = 'S7072xxx'
+data = LoadData(directory = directory, filename=filename)
+#data.convert_dat_to_npy(path_to_save_folder=path_to_save_folder, save_as_name=save_as_name)
+unfiltered_data, t = data.parse_dat()
+print(unfiltered_data)
 
 directory = '/Volumes/Macintosh HD/Users/gokceuzun/Desktop/4. SENE/Honors Project/S7072'
-filename = 'TAINI_1033_S7072_Drug2-2020_03_23-0000.npy'
-
+filename = 'S7072xxx.npy'
 data = LoadData(directory, filename)
 unfiltered_data = data.get_dat()
+print(unfiltered_data)
 
-fltr = Filter(unfiltered_data)
-filtered_data = fltr.butter_bandpass()
+#fltr = Filter(unfiltered_data)
+#filtered_data = np.array(fltr.butter_bandpass())
+#noise = fltr.noise_index
 
 #print(data)
 #print(unfiltered_data)
 
 #print(fltr)
-#print(filtered_data)
+#print(len(filtered_data))
+#print(len(noise))
 
 
 
